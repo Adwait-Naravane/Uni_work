9af0ec66a6466a2b38ff9ea62531195e87f2dca7
Adwait Naravane <adwaitnaravane@gmail.com>
2021-08-14 14:30:22 +0530

    differentiation

--- Autumn-2021/CS3101-D&A in C/garbage/trial.c
+++ Autumn-2021/CS3101-D&A in C/garbage/trial.c
@@ -12,8 +12,8 @@ double derive(double (*f)(double), double x0){
     double y2 = f(x2);
    return (y2-y1)/(x2-x1);
 }
-
 int main(){
-   printf("%f",derive(f, 1.0));
+   double der = derive(f, 1.0); 
+   printf("%f",der);
    return 0;
 }
